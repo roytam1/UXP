df6fcd35a769e1294e530afe12789b1b1e8676cb
wolfbeast <mcwerewolf@gmail.com>
2018-02-05 12:32:05 +0100

    Take forms out of the RSS reader feed.

--- toolkit/components/feeds/FeedProcessor.js
+++ toolkit/components/feeds/FeedProcessor.js
@@ -637,7 +637,8 @@ TextConstruct.prototype = {
     else
       return null;
 
-    return this.parserUtils.parseFragment(this.text, 0, isXML,
+    let flags = Ci.nsIParserUtils.SanitizerDropForms;
+    return this.parserUtils.parseFragment(this.text, flags, isXML,
                                           this.base, element);
   },
 
