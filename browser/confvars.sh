4f06bfcdca4de65850dc72d39ef24189631e7e55
wolfbeast <mcwerewolf@gmail.com>
2018-02-22 08:13:47 +0100

    Set BASILISK_VERSION override major.minor to 52.9

--- browser/confvars.sh
+++ browser/confvars.sh
@@ -18,7 +18,7 @@ if test "$OS_ARCH" = "WINNT"; then
   MOZ_MAINTENANCE_SERVICE=
 fi
 
-# For Basilisk we want to use 55.0.YYYY.MM.DD as MOZ_APP_VERSION in release
+# For Basilisk we want to use 52.9.YYYY.MM.DD as MOZ_APP_VERSION in release
 # builds so add-on developers have something to target while maintaining
 # Firefox compatiblity.
 # To enable add "export BASILISK_VERSION=1" to the .mozconfig file.
@@ -26,7 +26,7 @@ fi
 # don't export the variable if you are in development or don't care.
 # When not exported we fall back the value in the version*.txt file.
 if test -n "$BASILISK_VERSION" ; then
-    MOZ_APP_VERSION=55.0.`date --utc '+%Y.%m.%d'`
+    MOZ_APP_VERSION=52.9.`date --utc '+%Y.%m.%d'`
     MOZ_APP_VERSION_DISPLAY=`date --utc '+%Y.%m.%d'`
 else
     MOZ_APP_VERSION=`cat ${_topsrcdir}/$MOZ_BUILD_APP/config/version.txt`
