b6d9890c74203038b67e69da248026f4963c00ae
NTD <ntd@nonexist.net>
2018-02-24 09:49:34 -0500

    Developer Tools client should be a disable bool in ac configure

--- browser/confvars.sh
+++ browser/confvars.sh
@@ -56,14 +56,8 @@ MOZ_JSDOWNLOADS=1
 MOZ_RUST_MP4PARSE=1
 MOZ_RUST_URLPARSE=1
 MOZ_WEBEXTENSIONS=1
+MOZ_DEVTOOLS=1
 
 # Disable checking that add-ons are signed by the trusted root
 MOZ_ADDON_SIGNING=0
 MOZ_REQUIRE_SIGNING=0
-
-# Include the DevTools client, not just the server (which is the default)
-if test -n "$BASILISK_DISABLE_DEVTOOLS" ; then
-MOZ_DEVTOOLS=
-else
-MOZ_DEVTOOLS=1
-fi
