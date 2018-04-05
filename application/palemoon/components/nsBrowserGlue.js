9052cdd14f76eff3a40269ef8b5d1650d469a381
NTD <ntd@nonexist.net>
2018-04-05 07:03:38 -0400

    [PALEMOON] Remove check for MSE mixed prefs in nsBrowserGlue.js

--- application/palemoon/components/nsBrowserGlue.js
+++ application/palemoon/components/nsBrowserGlue.js
@@ -432,11 +432,6 @@ BrowserGlue.prototype = {
     
     LoginManagerParent.init();
     
-    // Make sure conflicting MSE prefs don't coexist
-    if (Services.prefs.getBoolPref('media.mediasource.format-reader', true)) {
-      Services.prefs.setBoolPref('media.mediasource.webm.enabled', false);
-    }
-
     Services.obs.notifyObservers(null, "browser-ui-startup-complete", "");
   },
 
