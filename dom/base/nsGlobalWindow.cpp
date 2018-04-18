7c9dda85db11f68b816e9802e3847fcf3d1ac0ae
janekptacijarabaci <janekptacijarabaci@seznam.cz>
2018-04-18 15:21:00 +0200

    Bug 1337814 - Remove rIC callback from pending callbacks before running it

--- dom/base/nsGlobalWindow.cpp
+++ dom/base/nsGlobalWindow.cpp
@@ -763,9 +763,8 @@ nsGlobalWindow::RunIdleRequest(IdleRequest* aRequest,
 {
   AssertIsOnMainThread();
   RefPtr<IdleRequest> request(aRequest);
-  nsresult result = request->IdleRun(AsInner(), aDeadline, aDidTimeout);
   RemoveIdleCallback(request);
-  return result;
+  return request->IdleRun(AsInner(), aDeadline, aDidTimeout);
 }
 
 nsresult
