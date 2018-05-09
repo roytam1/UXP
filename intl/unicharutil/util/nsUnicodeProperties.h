1aa30f609c048e92f43adfc88ae6d7f9d7820831
wolfbeast <mcwerewolf@gmail.com>
2018-05-09 17:52:17 +0200

    Font fallback should detect emoji range for color emoji font.

--- intl/unicharutil/util/nsUnicodeProperties.h
+++ intl/unicharutil/util/nsUnicodeProperties.h
@@ -60,6 +60,9 @@ enum EmojiPresentation {
   EmojiDefault = 2
 };
 
+const uint32_t kVariationSelector15 = 0xFE0E; // text presentation
+const uint32_t kVariationSelector16 = 0xFE0F; // emoji presentation
+
 // ICU is available, so simply forward to its API
 
 extern const hb_unicode_general_category_t sICUtoHBcategory[];
