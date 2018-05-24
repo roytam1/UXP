c00654154817acdf41c5ad8f12947186978c1c33
wolfbeast <mcwerewolf@gmail.com>
2018-05-24 19:53:20 +0200

    Remove CrashReporterParent leftover.
    Tag #20.

--- dom/plugins/ipc/PluginModuleParent.cpp
+++ dom/plugins/ipc/PluginModuleParent.cpp
@@ -55,7 +55,6 @@ using base::KillProcess;
 using mozilla::PluginLibrary;
 using mozilla::ipc::MessageChannel;
 using mozilla::ipc::GeckoChildProcessHost;
-using mozilla::dom::CrashReporterParent;
 
 using namespace mozilla;
 using namespace mozilla::plugins;
