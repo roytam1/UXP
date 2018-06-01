199c3900f88d1aa87a42853f976da30705947243
Ascrod <32915892+Ascrod@users.noreply.github.com>
2018-05-31 23:09:27 -0400

    Remove duplicate ifdefs in browser-safebrowsing.js

--- browser/base/content/browser-safebrowsing.js
+++ browser/base/content/browser-safebrowsing.js
@@ -2,7 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifdef MOZ_SAFE_BROWSING
 var gSafeBrowsing = {
 
   setReportPhishingMenu: function() {
@@ -47,4 +46,3 @@ var gSafeBrowsing = {
     return SafeBrowsing.getReportURL(name, gBrowser.currentURI);
   }
 }
-#endif
