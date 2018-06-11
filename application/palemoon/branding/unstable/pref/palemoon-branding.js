98b5fdb61bf8864581c7de2f0486c4dd9cd4e8cb
janekptacijarabaci <janekptacijarabaci@seznam.cz>
2018-06-11 20:37:14 +0200

    [suggestion] [PALEMOON] Unstable releases - fix some links on Linux
    Issue #480

--- application/palemoon/branding/unstable/pref/palemoon-branding.js
+++ application/palemoon/branding/unstable/pref/palemoon-branding.js
@@ -26,11 +26,19 @@ pref("app.update.promptWaitTime", 86400);
 
 // URL user can browse to manually if for some reason all update installation
 // attempts fail.
+#ifndef XP_LINUX
 pref("app.update.url.manual", "http://www.palemoon.org/unstable/");
-
+#else
+pref("app.update.url.manual", "http://linux.palemoon.org/download/unstable/");
+#endif
 // A default value for the "More information about this update" link
 // supplied in the "An update is available" page of the update wizard. 
+#ifndef XP_LINUX
 pref("app.update.url.details", "http://www.palemoon.org/unstable/");
+#else
+pref("app.update.url.details", "http://linux.palemoon.org/download/unstable/");
+#endif
+
 #else
 // Updates disabled (Mac, etc.)
 pref("app.update.enabled", false);
