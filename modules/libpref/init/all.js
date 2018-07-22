6bcca5706c16d141b5a4280e1aa5a0a91d381094
wolfbeast <mcwerewolf@gmail.com>
2018-07-22 10:53:21 +0200

    Allow AsyncDrawing for plugins in all versions.

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -2837,12 +2837,7 @@ pref("dom.ipc.plugins.unloadTimeoutSecs", 30);
 // Asynchronous plugin initialization is on hold.
 pref("dom.ipc.plugins.asyncInit.enabled", false);
 
-#ifdef RELEASE_OR_BETA
-pref("dom.ipc.plugins.asyncdrawing.enabled", false);
-#else
-// Allow the AsyncDrawing mode to be used for plugins in dev channels.
 pref("dom.ipc.plugins.asyncdrawing.enabled", true);
-#endif
 
 pref("dom.ipc.processCount", 1);
 
