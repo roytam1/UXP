84ae67b1bc5693025eef55a36e42833737c6c9f0
JustOff <Off.Just.Off@gmail.com>
2018-09-03 21:15:53 +0300

    Update 'browser.newtab.url' when it's used for new tabs and 'browser.startup.homepage' changes

---
+++ application/palemoon/components/preferences/newtaburl.js
@@ -0,0 +1,66 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+var gNewtabUrl = {
+  /**
+   * Writes browser.newtab.url with the appropriate value.
+   * If the choice is "my home page", get and sanitize
+   * the browser home page URL to make it suitable for newtab use.
+   *
+   * Called from prefwindow ondialogaccept in preferences.xul,
+   * newtabPage oncommand in tabs.xul, browserHomePage oninput,
+   * useCurrent, useBookmark and restoreDefaultHomePage oncommand
+   * in main.xul to consider instantApply.
+   */
+  writeNewtabUrl: function(newtabUrlChoice, browserHomepageUrl) {
+    try {
+      if (newtabUrlChoice) {
+        if (Services.prefs.getBoolPref("browser.preferences.instantApply")) {
+          newtabUrlChoice = parseInt(newtabUrlChoice);
+        } else {
+          return;
+        }
+      } else {
+        newtabUrlChoice = Services.prefs.getIntPref("browser.newtab.choice");
+      }
+      if (browserHomepageUrl || browserHomepageUrl == "") {
+        if (Services.prefs.getBoolPref("browser.preferences.instantApply")) {
+          if (browserHomepageUrl == "") {
+            browserHomepageUrl = "about:home";
+          }
+        } else {
+          return;
+        }
+      } else {
+        browserHomepageUrl = Services.prefs.getComplexValue("browser.startup.homepage",
+                              Components.interfaces.nsIPrefLocalizedString).data;
+      }
+      let newtabUrlPref = Services.prefs.getCharPref("browser.newtab.url");
+      switch (newtabUrlChoice) {
+        case 1:
+          newtabUrlPref = "about:logopage";
+          break;
+        case 2:
+          newtabUrlPref = "http://start.palemoon.org/";
+          break;
+        case 3:
+          // If url is a pipe-delimited set of pages, just take the first one.
+          let newtabUrlSanitizedPref=browserHomepageUrl.split("|")[0];
+          // XXX: do we need extra sanitation here, e.g. for invalid URLs?
+          Services.prefs.setCharPref("browser.newtab.myhome", newtabUrlSanitizedPref);
+          newtabUrlPref = newtabUrlSanitizedPref;
+          break;
+        case 4:
+          newtabUrlPref = "about:newtab";
+          break;
+        default:
+          // In case of any other value it's a custom URL, consider instantApply.
+          if (this.newtabPageCustom) {
+            newtabUrlPref = this.newtabPageCustom;
+          }
+      } 
+      Services.prefs.setCharPref("browser.newtab.url",newtabUrlPref);
+    } catch(e) { console.error(e); }
+  }
+};
--- application/palemoon/components/preferences/tabs.js
+++ application/palemoon/components/preferences/tabs.js
@@ -104,54 +104,5 @@ var gTabsPane = {
         // We need this to consider instantApply.
         this.newtabPageCustom = newtabUrlPref.value;
     }
-  },
-
-  /**
-   * Writes browser.newtab.url with the appropriate value.
-   * if the choice is "my home page", get and sanitize the browser home page
-   * URL to make it suitable for newtab use.
-   *
-   * Called from prefwindow's ondialogaccept handler and
-   * from browser.newtab.choice's oncommand to consider instantApply.
-   */
-  writeNewtabUrl: function(newtabUrlChoice) {
-    try {
-      if (newtabUrlChoice) {
-        if (Services.prefs.getBoolPref("browser.preferences.instantApply")) {
-          newtabUrlChoice = parseInt(newtabUrlChoice);
-        } else {
-          return;
-        }
-      } else {
-        newtabUrlChoice = Services.prefs.getIntPref("browser.newtab.choice");
-      }
-      let browserHomepageUrl = Services.prefs.getComplexValue("browser.startup.homepage",
-                                Components.interfaces.nsIPrefLocalizedString).data;
-      let newtabUrlPref = Services.prefs.getCharPref("browser.newtab.url");
-      switch (newtabUrlChoice) {
-        case 1:
-          newtabUrlPref = "about:logopage";
-          break;
-        case 2:
-          newtabUrlPref = "http://start.palemoon.org/";
-          break;
-        case 3:
-          // If url is a pipe-delimited set of pages, just take the first one.
-          let newtabUrlSanitizedPref=browserHomepageUrl.split("|")[0];
-          // XXX: do we need extra sanitation here, e.g. for invalid URLs?
-          Services.prefs.setCharPref("browser.newtab.myhome", newtabUrlSanitizedPref);
-          newtabUrlPref = newtabUrlSanitizedPref;
-          break;
-        case 4:
-          newtabUrlPref = "about:newtab";
-          break;
-        default:
-          // In case of any other value it's a custom URL, consider instantApply.
-          if (this.newtabPageCustom) {
-            newtabUrlPref = this.newtabPageCustom;
-          }
-      } 
-      Services.prefs.setCharPref("browser.newtab.url",newtabUrlPref);
-    } catch(e) { console.error(e); }
   }
 };
