d3a845d2fcd812d56ae05d6d263213a5b570a0ff
wolfbeast <mcwerewolf@gmail.com>
2018-09-11 14:24:50 +0200

    Bug 1469309 - Remove an unused sensor type.

--- hal/Hal.cpp
+++ hal/Hal.cpp
@@ -587,6 +587,7 @@ UnregisterSensorObserver(SensorType aSensor, ISensorObserver *aObserver) {
   AssertMainThread();
 
   if (!gSensorObservers) {
+    HAL_ERR("Un-registering a sensor when none have been registered");
     return;
   }
 
