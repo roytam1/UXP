349a05d4add7deeab9c940aac2f3b0ae8621e428
trav90 <travawine@palemoon.org>
2018-10-15 22:10:46 -0500

    Add AOMDecoder to AgnosticDecoderModule

--- dom/media/platforms/agnostic/AgnosticDecoderModule.cpp
+++ dom/media/platforms/agnostic/AgnosticDecoderModule.cpp
@@ -12,6 +12,10 @@
 #include "WAVDecoder.h"
 #include "TheoraDecoder.h"
 
+#ifdef MOZ_AV1
+#include "AOMDecoder.h"
+#endif
+
 namespace mozilla {
 
 bool
@@ -20,6 +24,9 @@ AgnosticDecoderModule::SupportsMimeType(const nsACString& aMimeType,
 {
   bool supports =
     VPXDecoder::IsVPX(aMimeType) ||
+#ifdef MOZ_AV1
+    AOMDecoder::IsAV1(aMimeType) ||
+#endif
     OpusDataDecoder::IsOpus(aMimeType) ||
     VorbisDataDecoder::IsVorbis(aMimeType) ||
     WaveDataDecoder::IsWave(aMimeType) ||
@@ -36,7 +43,13 @@ AgnosticDecoderModule::CreateVideoDecoder(const CreateDecoderParams& aParams)
 
   if (VPXDecoder::IsVPX(aParams.mConfig.mMimeType)) {
     m = new VPXDecoder(aParams);
-  } else if (TheoraDecoder::IsTheora(aParams.mConfig.mMimeType)) {
+  }
+#ifdef MOZ_AV1
+  else if (AOMDecoder::IsAV1(aParams.mConfig.mMimeType)) {
+    m = new AOMDecoder(aParams);
+  }
+#endif
+  else if (TheoraDecoder::IsTheora(aParams.mConfig.mMimeType)) {
     m = new TheoraDecoder(aParams);
   }
 
