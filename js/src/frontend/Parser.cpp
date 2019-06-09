e3244802ec276e312cf193f32ec83f026fd470d5
Gaming4JC <g4jc@hyperbola.info>
2019-06-09 17:20:09 -0400

    1340089 - Check the binding name in comprehensionFor.

--- js/src/frontend/Parser.cpp
+++ js/src/frontend/Parser.cpp
@@ -8101,6 +8101,8 @@ Parser<ParseHandler>::comprehensionFor(GeneratorKind comprehensionKind)
 
     MUST_MATCH_TOKEN_FUNC(TokenKindIsPossibleIdentifier, JSMSG_NO_VARIABLE_NAME);
     RootedPropertyName name(context, bindingIdentifier(YieldIsKeyword));
+    if (!name)
+        return null();
     if (name == context->names().let) {
         error(JSMSG_LET_COMP_BINDING);
         return null();
