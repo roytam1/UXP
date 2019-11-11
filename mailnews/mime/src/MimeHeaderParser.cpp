6c3fade999bed1e3ab9407a929e34858c5378c56
Matt A. Tobin <email@mattatobin.com>
2019-11-10 22:54:16 -0500

    Bug 1401858 - add null check to avoid crash in EncodedHeader()/DecodedHeader().
    Tag #1273

--- mailnews/mime/src/MimeHeaderParser.cpp
+++ mailnews/mime/src/MimeHeaderParser.cpp
@@ -10,7 +10,6 @@
 #include "nsMemory.h"
 #include "nsAutoPtr.h"
 #include "nsCOMPtr.h"
-#include "nsIMimeConverter.h"
 #include "nsIMsgHeaderParser.h"
 
 namespace mozilla {
@@ -76,6 +75,7 @@ nsCOMArray<msgIAddressObject> DecodedHeader(const nsAString &aHeader)
     return retval;
   }
   nsCOMPtr<nsIMsgHeaderParser> headerParser(services::GetHeaderParser());
+  NS_ENSURE_TRUE(headerParser, retval);
   msgIAddressObject **addresses = nullptr;
   uint32_t length;
   nsresult rv = headerParser->ParseDecodedHeader(aHeader, false,
@@ -95,6 +95,7 @@ nsCOMArray<msgIAddressObject> EncodedHeader(const nsACString &aHeader,
     return retval;
   }
   nsCOMPtr<nsIMsgHeaderParser> headerParser(services::GetHeaderParser());
+  NS_ENSURE_TRUE(headerParser, retval);
   msgIAddressObject **addresses = nullptr;
   uint32_t length;
   nsresult rv = headerParser->ParseEncodedHeader(aHeader, aCharset,
