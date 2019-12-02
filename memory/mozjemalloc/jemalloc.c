edb91248b1a101b6e11df5d4e99689924f5d0496
wolfbeast <mcwerewolf@wolfbeast.com>
2019-12-02 09:54:40 +0100

    Issue #1307 - Part 8: Remove deprecated sysctl.h inclusion.

--- memory/mozjemalloc/jemalloc.c
+++ memory/mozjemalloc/jemalloc.c
@@ -284,9 +284,6 @@ typedef long ssize_t;
 #include <sys/param.h>
 #include <sys/time.h>
 #include <sys/types.h>
-#if !defined(MOZ_MEMORY_SOLARIS) && !defined(MOZ_MEMORY_ANDROID)
-#include <sys/sysctl.h>
-#endif
 #include <sys/uio.h>
 
 #include <errno.h>
