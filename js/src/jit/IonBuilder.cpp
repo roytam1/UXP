06b3cb6ccd352946a428d5942c646fc4844225b6
Gaming4JC <g4jc@hyperbola.info>
2019-12-14 09:22:32 -0500

    Bug 1331092 - Part 8: Support JSOP_TOASYNCITER in JIT.
    Tag #1287

--- js/src/jit/IonBuilder.cpp
+++ js/src/jit/IonBuilder.cpp
@@ -2133,6 +2133,9 @@ IonBuilder::inspectOpcode(JSOp op)
       case JSOP_TOASYNCGEN:
         return jsop_toasyncgen();
 
+      case JSOP_TOASYNCITER:
+        return jsop_toasynciter();
+
       case JSOP_TOID:
         return jsop_toid();
 
@@ -13203,6 +13206,20 @@ IonBuilder::jsop_toasyncgen()
     MOZ_ASSERT(unwrapped->type() == MIRType::Object);
 
     MToAsyncGen* ins = MToAsyncGen::New(alloc(), unwrapped);
+
+    current->add(ins);
+    current->push(ins);
+
+    return resumeAfter(ins);
+}
+
+bool
+IonBuilder::jsop_toasynciter()
+{
+    MDefinition* unwrapped = current->pop();
+    MOZ_ASSERT(unwrapped->type() == MIRType::Object);
+
+    MToAsyncIter* ins = MToAsyncIter::New(alloc(), unwrapped);
 
     current->add(ins);
     current->push(ins);
--- js/src/jit/shared/LOpcodes-shared.h
+++ js/src/jit/shared/LOpcodes-shared.h
@@ -354,6 +354,7 @@
     _(TypeOfV)                      \
     _(ToAsync)                      \
     _(ToAsyncGen)                   \
+    _(ToAsyncIter)                  \
     _(ToIdV)                        \
     _(Floor)                        \
     _(FloorF)                       \
