b70a9b1d12e8b737a498883aa5436d512d97a88b
Gaming4JC <g4jc@hyperbola.info>
2020-01-20 18:13:15 -0500

    Bug 1405335 - Remove custom element attached and detached callbacks validation checks
    Tag UXP Issue #1344

--- dom/base/CustomElementRegistry.cpp
+++ dom/base/CustomElementRegistry.cpp
@@ -523,9 +523,7 @@ static const char* kLifeCycleCallbackNames[] = {
   "adoptedCallback",
   "attributeChangedCallback",
   // The life cycle callbacks from v0 spec.
-  "createdCallback",
-  "attachedCallback",
-  "detachedCallback"
+  "createdCallback"
 };
 
 static void
