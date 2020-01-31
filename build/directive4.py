fced04faa220dff0582a7e86d29d40ce754de5ed
wolfbeast <mcwerewolf@wolfbeast.com>
2020-01-31 16:54:07 +0100

    Issue #1342 - Remove support for system libvpx

--- build/directive4.py
+++ build/directive4.py
@@ -36,7 +36,6 @@ if ('MOZ_OFFICIAL_BRANDING' in listConfig) or (strBrandingDirectory.endswith("br
             'MOZ_SYSTEM_JPEG',
             'MOZ_SYSTEM_ZLIB',
             'MOZ_SYSTEM_BZ2',
-            'MOZ_SYSTEM_LIBVPX',
             'MOZ_SYSTEM_JEMALLOC'
         ]
         
