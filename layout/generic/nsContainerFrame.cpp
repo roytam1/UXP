ce621f34df2ecf9f69e21fe0298646a98f0555de
wolfbeast <mcwerewolf@wolfbeast.com>
2020-04-10 21:48:39 +0200

    Issue #1511 - Bail on orphan situation.
    This resolves #1511

--- layout/generic/nsContainerFrame.cpp
+++ layout/generic/nsContainerFrame.cpp
@@ -1923,6 +1923,10 @@ nsContainerFrame::RenumberFrameAndDescendants(int32_t* aOrdinal,
             nsIFrame *f = bullet;
             do {
               nsIFrame *parent = f->GetParent();
+              if (!parent) {
+                // We may have an orphan situation in some corner cases.
+                break;
+              }
               parent->ChildIsDirty(f);
               f = parent;
             } while (f != listItem);
