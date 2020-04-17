5a9d76e6d51f5cd98d82081dc66e801cc2d4cee1
Matt A. Tobin <email@mattatobin.com>
2020-04-16 20:02:55 -0400

    Bug 1347640 - HTMLInputElement shouldn't try to cancel image request on all the type changes
    * Move the changing of HTMLInputElement's mType from ParseAttribute to AfterSetAttr
    * Move a few more things from AfterSetAttr to HandleTypeChange
    
    Tag #1375

--- dom/html/HTMLInputElement.h
+++ dom/html/HTMLInputElement.h
@@ -1100,7 +1100,7 @@ protected:
   /**
    * Manages the internal data storage across type changes.
    */
-  void HandleTypeChange(uint8_t aNewType);
+  void HandleTypeChange(uint8_t aNewType, bool aNotify);
 
   /**
    * Sanitize the value of the element depending of its current type.
