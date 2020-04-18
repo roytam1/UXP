241c3b3200dc1c659986b736cf65daa2881bb4c5
Matt A. Tobin <email@mattatobin.com>
2020-04-18 13:10:09 -0400

    Issue #80 - De-unify layout/forms

--- layout/forms/nsHTMLButtonControlFrame.cpp
+++ layout/forms/nsHTMLButtonControlFrame.cpp
@@ -7,6 +7,7 @@
 
 #include "nsContainerFrame.h"
 #include "nsIFormControlFrame.h"
+#include "nsIFrameInlines.h"
 #include "nsPresContext.h"
 #include "nsGkAtoms.h"
 #include "nsButtonFrameRenderer.h"
