5386cd812b9fe8294a57eae7ed6d2ef117912af4
wolfbeast <mcwerewolf@wolfbeast.com>
2020-05-30 21:14:43 +0200

    Issue #1570 - Implement globalThis
    This resolves #1570

--- js/src/vm/CommonPropertyNames.h
+++ js/src/vm/CommonPropertyNames.h
@@ -160,6 +160,7 @@
     macro(getPropertyDescriptor, getPropertyDescriptor, "getPropertyDescriptor") \
     macro(getPrototypeOf, getPrototypeOf, "getPrototypeOf") \
     macro(global, global, "global") \
+    macro(globalThis, globalThis, "globalThis") \
     macro(Handle, Handle, "Handle") \
     macro(has, has, "has") \
     macro(hasOwn, hasOwn, "hasOwn") \
