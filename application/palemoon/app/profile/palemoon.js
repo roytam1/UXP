61e78cda6a5120371812ba3331868f9094be86b6
Moonchild <moonchild@palemoon.org>
2020-06-23 14:48:26 +0000

    [Pale-Moon] Issue #1782 - Add option to insert all new tabs after the current tab
    This adds 'browser.tabs.insertAllAfterCurrent' as a preference which
    makes all new tabs opened to be next to the currently active tab, instead
    of at the end - regardless if the tab is considered "related" to the active
    tab. This (naturally and logically) overrides 'insertRelatedAfterCurrent'
    
    Resolves #1782

--- application/palemoon/app/profile/palemoon.js
+++ application/palemoon/app/profile/palemoon.js
@@ -433,6 +433,7 @@ pref("browser.link.open_newwindow.disabled_in_fullscreen", false);
 // Tabbed browser
 pref("browser.tabs.autoHide", false);
 pref("browser.tabs.closeWindowWithLastTab", true);
+pref("browser.tabs.insertAllAfterCurrent", false);
 pref("browser.tabs.insertRelatedAfterCurrent", true);
 pref("browser.tabs.warnOnClose", true);
 pref("browser.tabs.warnOnCloseOtherTabs", true);
