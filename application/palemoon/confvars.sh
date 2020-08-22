0442dd4ee579d4cc1b9d8fb3ef3bc66fe6df4148
Matt A. Tobin <email@mattatobin.com>
2020-08-22 09:49:44 -0400

    [Pale-Moon] Add a configure option for Phoenix Extensions Dual-GUID System and set default off in confvars

--- application/palemoon/confvars.sh
+++ application/palemoon/confvars.sh
@@ -65,7 +65,7 @@ MOZ_DEVTOOLS=1
 # Allows installation of Firefox GUID targeted extensions despite having
 # a different Application ID
 # On UXP this is a possible feature only for the Tycho Add-ons Manager
-MOZ_PHOENIX_EXTENSIONS=1
+MOZ_PHOENIX_EXTENSIONS=
 
 # Platform Feature: Sync Service
 MOZ_SERVICES_COMMON=1
