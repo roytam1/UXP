abb3630983aab709ceaf55f72842783b4e52364e
Moonchild <moonchild@palemoon.org>
2020-09-16 16:39:15 +0000

    Issue #1643 - Part 2: Implement ResizeObserver API
    Implements ResizeObserver, ResizeObserverEntry and ResizeObservation

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -2652,6 +2652,9 @@ pref("layout.css.font-loading-api.enabled", true);
 // Should stray control characters be rendered visibly?
 pref("layout.css.control-characters.visible", false);
 
+// Is support for ResizeObservers enabled?
+pref("layout.css.resizeobserver.enabled", true);
+
 // pref for which side vertical scrollbars should be on
 // 0 = end-side in UI direction
 // 1 = end-side in document/content direction
