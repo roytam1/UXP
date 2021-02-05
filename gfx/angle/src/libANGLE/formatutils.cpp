8fe6e9636d23b42cda9f070d2f4c80f978343291
Moonchild <moonchild@palemoon.org>
2021-02-05 18:40:41 +0000

    [angle] Cherry-pick compressed tex depth stride fix.

--- gfx/angle/src/libANGLE/formatutils.cpp
+++ gfx/angle/src/libANGLE/formatutils.cpp
@@ -863,13 +863,22 @@ gl::ErrorOrResult<GLuint> InternalFormat::computeDepthPitch(GLenum formatType,
                                                             GLint rowLength,
                                                             GLint imageHeight) const
 {
-    GLuint rows =
-        (imageHeight > 0 ? static_cast<GLuint>(imageHeight) : static_cast<GLuint>(height));
+    CheckedNumeric<GLuint> pixelsHeight(imageHeight > 0 ? static_cast<GLuint>(imageHeight)
+                                                        : static_cast<GLuint>(height));
+
+    CheckedNumeric<GLuint> rowCount;
+    if (compressed) {
+        CheckedNumeric<GLuint> checkedBlockHeight(compressedBlockHeight);
+        rowCount = (pixelsHeight + checkedBlockHeight - 1u) / checkedBlockHeight;
+    } else {
+        rowCount = pixelsHeight;
+    }
+
     GLuint rowPitch = 0;
     ANGLE_TRY_RESULT(computeRowPitch(formatType, width, alignment, rowLength), rowPitch);
 
     CheckedNumeric<GLuint> checkedRowPitch(rowPitch);
-    auto depthPitch = checkedRowPitch * rows;
+    auto depthPitch = checkedRowPitch * rowCount;
     ANGLE_TRY_CHECKED_MATH(depthPitch);
     return depthPitch.ValueOrDie();
 }
