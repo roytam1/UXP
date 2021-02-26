99cde7ce7b4bb6bee9412f671748d16701d0ab9a
Gaming4JC <g4jc@hyperbola.info>
2021-02-26 11:57:24 -0500

    [widget] MailNews Followup: Require user interaction when picking files or folders v2
    98f6ad61f4f3c66209036db4bc7a4ae6ff4d1d3d introduced a boolean, this just passes it down and fixes build bustage of mailnews.

--- mailnews/addrbook/src/nsAbManager.cpp
+++ mailnews/addrbook/src/nsAbManager.cpp
@@ -544,7 +544,7 @@ NS_IMETHODIMP nsAbManager::ExportAddressBook(mozIDOMWindowProxy *aParentWin, nsI
                                     ArrayLength(formatStrings), getter_Copies(title));
   NS_ENSURE_SUCCESS(rv, rv);
 
-  rv = filePicker->Init(aParentWin, title, nsIFilePicker::modeSave);
+  rv = filePicker->Init(aParentWin, title, nsIFilePicker::modeSave, false);
   NS_ENSURE_SUCCESS(rv, rv);
 
   filePicker->SetDefaultString(dirName);
