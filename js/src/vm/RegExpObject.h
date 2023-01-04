86782b8f397a83fd775d73a1c8c2f0cf5f97bd14
Martok <martok@martoks-place.de>
2023-01-04 20:48:25 +0100

    Issue #1382 - RegExpShared::groupsTemplate_ cannot be a GCPtr since RegExpShared is managed by C++

--- js/src/vm/RegExpObject.h
+++ js/src/vm/RegExpObject.h
@@ -134,8 +134,8 @@ class RegExpShared
     bool               canStringMatch;
     bool               marked_;
 
-    uint32_t            numNamedCaptures_;
-    GCPtr<PlainObject*> groupsTemplate_;
+    uint32_t              numNamedCaptures_;
+    HeapPtr<PlainObject*> groupsTemplate_;
 
     RegExpCompilation  compilationArray[4];
 
