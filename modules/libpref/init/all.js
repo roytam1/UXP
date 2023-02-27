87b94f83ca7066b2e67898f0c218e2d64c0e1863
Moonchild <moonchild@palemoon.org>
2023-02-28 00:40:26 +0100

    No Issue - Enable MSE for WebM on all platforms.
    WebM is certainly mature enough to have hooked into MSE (default on
    Linux and other for years now).

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -582,14 +582,8 @@ pref("media.track.enabled", false);
 
 // Whether to enable MediaSource support.
 pref("media.mediasource.enabled", true);
-
 pref("media.mediasource.mp4.enabled", true);
-
-#if defined(XP_WIN) || defined(XP_MACOSX)
-pref("media.mediasource.webm.enabled", false);
-#else
 pref("media.mediasource.webm.enabled", true);
-#endif
 pref("media.mediasource.webm.audio.enabled", true);
 
 #ifdef MOZ_AV1
