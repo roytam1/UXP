be916ef7caa9fd1b2ffa69d0d0f59ca7c6ae7b29
Brian Smith <brian@dbsoft.org>
2023-04-21 17:55:33 -0500

    Issue #1691 - Part 9: Make import() work when the active script is in another document. https://bugzilla.mozilla.org/show_bug.cgi?id=1342012 Associate event handler with active script when they are compiled.
    (cherry picked from commit 1a3cc5d6828a05a6309d959fad3c7e959ab9eb8d)

--- dom/events/EventListenerManager.cpp
+++ dom/events/EventListenerManager.cpp
@@ -22,6 +22,8 @@
 #include "mozilla/dom/Element.h"
 #include "mozilla/dom/Event.h"
 #include "mozilla/dom/EventTargetBinding.h"
+#include "mozilla/dom/ModuleScript.h"
+#include "mozilla/dom/ScriptLoader.h"
 #include "mozilla/dom/TouchEvent.h"
 #include "mozilla/TimelineConsumers.h"
 #include "mozilla/EventTimelineMarker.h"
@@ -1017,6 +1019,15 @@ EventListenerManager::CompileEventHandlerInternal(Listener* aListener,
                                       argCount, argNames, *body, handler.address());
   NS_ENSURE_SUCCESS(result, result);
   NS_ENSURE_TRUE(handler, NS_ERROR_FAILURE);
+
+  JS::Rooted<JSFunction*> func(cx, JS_GetObjectFunction(handler));
+  MOZ_ASSERT(func);
+  JS::Rooted<JSScript*> jsScript(cx, JS_GetFunctionScript(cx, func));
+  MOZ_ASSERT(jsScript);
+  RefPtr<LoadedScript> loaderScript = ScriptLoader::GetActiveScript(cx);
+  if (loaderScript) {
+    loaderScript->AssociateWithScript(jsScript);
+  }
 
   if (jsEventHandler->EventName() == nsGkAtoms::onerror && win) {
     RefPtr<OnErrorEventHandlerNonNull> handlerCallback =
