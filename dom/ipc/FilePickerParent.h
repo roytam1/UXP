a3e6917887d3e494c14920b09b0ba722b7257eed
roytam1 <roytam@gmail.com>
2023-09-08 08:02:31 +0800

    Revert "[DOM] Make IORunnable::mFilePickerParent into a RefPtr."
    This reverts commit 0359d8a9e73968b3ed825a4f1e88ea4d894595fe.

--- dom/ipc/FilePickerParent.h
+++ dom/ipc/FilePickerParent.h
@@ -69,7 +69,7 @@ class FilePickerParent : public PFilePickerParent
 
   private:
     virtual ~FilePickerShownCallback() {}
-    RefPtr<FilePickerParent> mFilePickerParent;
+    FilePickerParent* mFilePickerParent;
   };
 
  private:
@@ -78,7 +78,7 @@ class FilePickerParent : public PFilePickerParent
   // This runnable is used to do some I/O operation on a separate thread.
   class IORunnable : public Runnable
   {
-    RefPtr<FilePickerParent> mFilePickerParent;
+    FilePickerParent* mFilePickerParent;
     nsTArray<nsCOMPtr<nsIFile>> mFiles;
     nsTArray<BlobImplOrString> mResults;
     nsCOMPtr<nsIEventTarget> mEventTarget;
