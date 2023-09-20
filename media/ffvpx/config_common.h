4610b9fd55ade7719341ecb96f16f0b3dd1a0601
trav90 <travawine@palemoon.org>
2023-09-20 13:27:07 -0500

    Issue #2311 - Part 1 - Add ffmpeg's own floating point real FFT functions to ffvpx
    Instead of using Libav's.

--- media/ffvpx/config_common.h
+++ media/ffvpx/config_common.h
@@ -1,4 +1,12 @@
 #ifndef MOZ_FFVPX_CONFIG_COMMON_H
 #define MOZ_FFVPX_CONFIG_COMMON_H
 #include "defaults_disabled.h"
+
+#ifdef MOZ_LIBAV_FFT
+#undef CONFIG_FFT
+#undef CONFIG_RDFT
+#define CONFIG_FFT 0
+#define CONFIG_RDFT 1
+#endif
+
 #endif
