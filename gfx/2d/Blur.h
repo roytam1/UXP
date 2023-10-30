8d30faf95fc668a37b2a6362aef8eefc84d77536
Moonchild <moonchild@palemoon.org>
2023-10-30 12:23:36 +0100

    Issue #2364 - Re-factor AlphaBoxBlur
    Only blur one quadrant of a box-shadow and mirror it to the other quadrants.
    This applies only if the corners are symmetrical (square corners or equal
    corner radii) otherwise we'll fall back to the old method.

--- gfx/2d/Blur.h
+++ gfx/2d/Blur.h
@@ -68,6 +68,14 @@ public:
                float aSigmaX,
                float aSigmaY);
 
+  AlphaBoxBlur();
+
+  void Init(const Rect& aRect,
+            const IntSize& aSpreadRadius,
+            const IntSize& aBlurRadius,
+            const Rect* aDirtyRect,
+            const Rect* aSkipRect);
+
   ~AlphaBoxBlur();
 
   /**
