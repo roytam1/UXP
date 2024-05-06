c77b4b0badd092f16e1426f595c2c78f3a3f7fb9
Moonchild <moonchild@palemoon.org>
2024-05-06 22:09:18 +0200

    Issue #2510 - Unprefix -moz-fit-content
    No aliasing since this has been out for a while.
    Devtools db regenerated with `mach devtools-css-db`
    
    Resolves #2510

--- layout/tables/BasicTableLayoutStrategy.cpp
+++ layout/tables/BasicTableLayoutStrategy.cpp
@@ -179,7 +179,7 @@ GetISizeInfo(nsRenderingContext *aRenderingContext,
         if (!aIsCell || maxISize.GetIntValue() == NS_STYLE_WIDTH_AVAILABLE) {
             maxISize.SetNoneValue();
         } else if (maxISize.GetIntValue() == NS_STYLE_WIDTH_FIT_CONTENT) {
-            // for 'max-inline-size', '-moz-fit-content' is like
+            // for 'max-inline-size', 'fit-content' is like
             // 'max-content'
             maxISize.SetIntValue(NS_STYLE_WIDTH_MAX_CONTENT,
                                  eStyleUnit_Enumerated);
@@ -206,7 +206,7 @@ GetISizeInfo(nsRenderingContext *aRenderingContext,
         if (!aIsCell || minISize.GetIntValue() == NS_STYLE_WIDTH_AVAILABLE) {
             minISize.SetCoordValue(0);
         } else if (minISize.GetIntValue() == NS_STYLE_WIDTH_FIT_CONTENT) {
-            // for 'min-inline-size', '-moz-fit-content' is like
+            // for 'min-inline-size', 'fit-content' is like
             // 'min-content'
             minISize.SetIntValue(NS_STYLE_WIDTH_MIN_CONTENT,
                                  eStyleUnit_Enumerated);
