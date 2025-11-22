34d1d5a65063ea4cc461778fa6a5bffccff0ebd2
Moonchild <moonchild@palemoon.org>
2025-11-22 17:46:36 +0100

    Issue #2869 - Make Purple Buffer additions check if cell is tenured.
    This should avoid a CTD if `cell` is not tenured by moving this check to
    CC instead of having it fatally fail at marking grey.
    
    Resolves #2869

--- js/src/jsgc.cpp
+++ js/src/jsgc.cpp
@@ -7138,6 +7138,7 @@ JS_PUBLIC_API(bool)
 js::gc::detail::CellIsMarkedGrayIfKnown(const Cell* cell)
 {
     MOZ_ASSERT(cell);
+    // Cells should always be tenured cells, here.
     if (!cell || !cell->isTenured())
         return false;
 
