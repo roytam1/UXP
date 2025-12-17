b9ded9c3dc10b0a75631d61547a018cfe42c3066
Basilisk-Dev <basiliskdev@protonmail.com>
2025-12-17 16:56:25 -0500

    Issue #2306: Fix spacing

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -1340,6 +1340,8 @@ pref("javascript.options.dynamicImport", true);
 
 // Streams API
 pref("javascript.options.streams", true);
+
+// Enable garbage collection of weakrefed objects
 pref("javascript.options.weakrefs", false);
 
 // advanced prefs
