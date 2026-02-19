69e5594d45dcc7b288224e74b2d919e76a859ad3
Job Bautista <jobbautista9@protonmail.com>
2026-02-19 23:08:55 +0800

    Issue #2953 - Part 2: Don't attempt to continue decoding if no next keyframe.
    When we have no future keyframe and an error occurred, we can't skip over
    anything and we shouldn't try to simply decode the next video frame; so we
    abort early.

--- dom/media/MediaFormatReader.cpp
+++ dom/media/MediaFormatReader.cpp
@@ -1620,6 +1620,9 @@ MediaFormatReader::Update(TrackType aTrack)
       return;
     } else if (aTrack == TrackType::kAudioTrack) {
       decoder.Flush();
+    } else {
+      // We can't recover from this error.
+      NotifyError(aTrack, NS_ERROR_DOM_MEDIA_FATAL_ERR);
     }
   }
 
