4c61d6eca0c60beb749cfbb7ae52c8bf4f58831b
Job Bautista <jobbautista9@protonmail.com>
2026-02-19 14:20:23 +0800

    Issue #2949 - Part 1: Add option to disable hardware decoding.

--- dom/media/platforms/PlatformDecoderModule.h
+++ dom/media/platforms/PlatformDecoderModule.h
@@ -10,6 +10,7 @@
 #include "ImageContainer.h"
 #include "MediaDecoderReader.h"
 #include "MediaInfo.h"
+#include "mozilla/EnumSet.h"
 #include "mozilla/MozPromise.h"
 #include "mozilla/layers/LayersTypes.h"
 #include "mozilla/layers/KnowsCompositor.h"
@@ -50,6 +51,13 @@ struct MOZ_STACK_CLASS CreateDecoderParams final {
     : mConfig(aConfig)
   {}
 
+  enum class Option
+  {
+    Default,
+    HardwareDecoderNotAllowed,
+  };
+  using OptionSet = EnumSet<Option>;
+
   template <typename T1, typename... Ts>
   CreateDecoderParams(const TrackInfo& aConfig, T1&& a1, Ts&&... args)
     : mConfig(aConfig)
@@ -88,6 +96,7 @@ struct MOZ_STACK_CLASS CreateDecoderParams final {
   RefPtr<GMPCrashHelper> mCrashHelper;
 #endif
   bool mUseBlankDecoder = false;
+  OptionSet mOptions = OptionSet(Option::Default);
 
 private:
   void Set(TaskQueue* aTaskQueue) { mTaskQueue = aTaskQueue; }
@@ -99,6 +108,7 @@ private:
   void Set(GMPCrashHelper* aCrashHelper) { mCrashHelper = aCrashHelper; }
 #endif
   void Set(bool aUseBlankDecoder) { mUseBlankDecoder = aUseBlankDecoder; }
+  void Set(OptionSet aOptions) { mOptions = aOptions; }
   void Set(layers::KnowsCompositor* aKnowsCompositor) { mKnowsCompositor = aKnowsCompositor; }
   template <typename T1, typename T2, typename... Ts>
   void Set(T1&& a1, T2&& a2, Ts&&... args)
