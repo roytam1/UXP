01b4ef076f2bfea2669967a97c10abb033d15775
Moonchild <moonchild@palemoon.org>
2026-03-09 11:30:38 +0100

    No issue - Fix some fatal printing assertions.
    - Turn a fatal assert into a warning if we can't guarantee a drawTarget.
      This should not be fatal since the DrawTarget is checked later.
    - Remove a fatal assertion if a display table background is cross-document
      since non-display iframes may cause this situation without it being an
      error.

--- layout/tables/nsTableFrame.h
+++ layout/tables/nsTableFrame.h
@@ -81,9 +81,6 @@ class nsDisplayTableBackgroundSet {
   nsDisplayTableBackgroundSet(nsDisplayListBuilder* aBuilder, nsIFrame* aTable)
       : mBuilder(aBuilder) {
     mPrevTableBackgroundSet = mBuilder->SetTableBackgroundSet(this);
-    mozilla::DebugOnly<const nsIFrame*> reference =
-        mBuilder->FindReferenceFrameFor(aTable, &mToReferenceFrame);
-    MOZ_ASSERT(nsLayoutUtils::IsAncestorFrameCrossDoc(reference, aTable));
     mDirtyRect = mBuilder->GetDirtyRect();
   }
 
