f0a31850dead7aed385d0e78d50ff0ae38261831
ownedbywuigi <ownedbywuigi@gmail.com>
2026-03-29 21:45:49 +0100

    Issue #3030 - Block auth prompt if there is a sync XHR active.
    This attempts to fix https://forum.palemoon.org/viewtopic.php?f=3&t=33260

--- dom/xhr/XMLHttpRequestMainThread.cpp
+++ dom/xhr/XMLHttpRequestMainThread.cpp
@@ -3544,6 +3544,13 @@ XMLHttpRequestMainThread::ShouldBlockAuthPrompt()
   // Verify that it's ok to prompt for credentials here, per spec
   // http://xhr.spec.whatwg.org/#the-send%28%29-method
 
+  // Privileged add-ons can still issue synchronous XHR from chrome context.
+  // Showing a blocking HTTP auth dialog in that path can deadlock the UI when
+  // extension code is already running inside a nested event loop.
+  if (mFlagSynchronous && IsSystemXHR()) {
+    return true;
+  }
+
   if (mAuthorRequestHeaders.Has("authorization")) {
     return true;
   }
