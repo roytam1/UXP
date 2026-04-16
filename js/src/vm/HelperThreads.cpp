52b3fada95088f6150ef5f0527e17a420146fbbe
Basilisk-Dev <basiliskdev@protonmail.com>
2026-04-16 12:10:10 -0400

    Issue #3050 - Backport 1431353: Remove off-thread parse thread limit

--- js/src/vm/HelperThreads.cpp
+++ js/src/vm/HelperThreads.cpp
@@ -927,11 +927,7 @@ GlobalHelperThreadState::maxParseThreads() const
     if (IsHelperThreadSimulatingOOM(js::oom::THREAD_TYPE_PARSE))
         return 1;
 
-    // Don't allow simultaneous off thread parses, to reduce contention on the
-    // atoms table. Note that wasm compilation depends on this to avoid
-    // stalling the helper thread, as off thread parse tasks can trigger and
-    // block on other off thread wasm compilation tasks.
-    return 1;
+    return threadCount;
 }
 
 size_t
