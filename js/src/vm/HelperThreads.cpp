38dc7b394e145fd3d488734f9995015ff29f46c5
Moonchild <moonchild@palemoon.org>
2026-04-29 12:10:41 +0200

    Issue #3066 - Follow-up: Reserve threads for burst use cases.

--- js/src/vm/HelperThreads.cpp
+++ js/src/vm/HelperThreads.cpp
@@ -926,8 +926,12 @@ GlobalHelperThreadState::maxParseThreads() const
 {
     if (IsHelperThreadSimulatingOOM(js::oom::THREAD_TYPE_PARSE))
         return 1;
-
-    return threadCount;
+    // On very old hardware with low core count, use physical core count.
+    if (cpuCount <= 2)
+        return cpuCount;
+    // Reserve kernel threads so we don't end up deadlocking with burst use.
+    // If physical core count > 2 this is always at least 2, even without HT.
+    return threadCount - 1;
 }
 
 size_t
