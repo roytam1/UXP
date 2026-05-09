4510bf92a26c811557d0cfd8bc2728595ab4438b
Basilisk-Dev <basiliskdev@protonmail.com>
2026-05-09 12:44:44 -0400

    Floor fractional border widths

--- layout/style/nsStyleStruct.h
+++ layout/style/nsStyleStruct.h
@@ -1145,10 +1145,10 @@ private:
   nsCSSShadowItem mArray[1]; // This MUST be the last item
 };
 
-// Border widths are rounded to the nearest integer number of pixels, but values
-// between zero and one device pixels are always rounded up to one device pixel.
+// Border widths are rounded down to integer pixels, but values between zero and
+// one device pixel are always rounded up to one device pixel.
 #define NS_ROUND_BORDER_TO_PIXELS(l,tpp) \
-  ((l) == 0) ? 0 : std::max((tpp), ((l) + ((tpp) / 2)) / (tpp) * (tpp))
+  ((l) == 0) ? 0 : std::max((tpp), (l) / (tpp) * (tpp))
 // Caret widths are rounded to the nearest-below integer number of pixels, but values
 // between zero and one device pixels are always rounded up to one device pixel.
 #define NS_ROUND_CARET_TO_PIXELS(l,tpp) \
