2bd46d46068d7a2135e92210860f4fbf42671bb1
Basilisk-Dev <basiliskdev@protonmail.com>
2026-05-11 10:52:21 -0400

    Issue #888 - Enable AV1 playback by default

--- dom/media/MediaPrefs.h
+++ dom/media/MediaPrefs.h
@@ -115,7 +115,7 @@ private:
   DECL_MEDIA_PREF("media.ffvpx.enabled",                      PDMFFVPXEnabled, bool, true);
 #endif
 #ifdef MOZ_AV1
-  DECL_MEDIA_PREF("media.av1.enabled",                        AV1Enabled, bool, false);
+  DECL_MEDIA_PREF("media.av1.enabled",                        AV1Enabled, bool, true);
 #endif
 #ifdef XP_WIN
   DECL_MEDIA_PREF("media.wmf.enabled",                        PDMWMFEnabled, bool, true);
