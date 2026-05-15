80c31f4b0147fe501fcc1215d845a30a264c0c66
roytam1 <roytam@gmail.com>
2026-05-15 09:30:18 +0800

    dom/html: remove my hackfix in HTMLInputElement::StartRangeThumbDrag()

--- dom/html/HTMLInputElement.cpp
+++ dom/html/HTMLInputElement.cpp
@@ -4122,7 +4122,6 @@ HTMLInputElement::StartRangeThumbDrag(WidgetGUIEvent* aEvent)
   // of the thumb.
   nsIPresShell::SetCapturingContent(this, CAPTURE_IGNOREALLOWED);
   nsRangeFrame* rangeFrame = do_QueryFrame(GetPrimaryFrame());
-  if(!rangeFrame) return;
 
   // Before we change the value, record the current value so that we'll
   // correctly send a 'change' event if appropriate. We need to do this here
