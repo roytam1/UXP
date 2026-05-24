d582d12ad821038014c5759565176926d3c672eb
Basilisk-Dev <basiliskdev@protonmail.com>
2026-05-24 14:58:21 -0400

    Revert "Issue #3092 - Refactor WASM compilation handling"
    This reverts commit 1c09cd7816cbfefd5b4a7afa770e15edb9a02919.

--- js/src/wasm/WasmGenerator.h
+++ js/src/wasm/WasmGenerator.h
@@ -104,11 +104,9 @@ class MOZ_STACK_CLASS ModuleGenerator
 
     // Parallel compilation
     bool                            parallel_;
-    bool                            parallelCompilationInProgressOnHelperThread_;
     uint32_t                        outstanding_;
     IonCompileTaskVector            tasks_;
     IonCompileTaskPtrVector         freeTasks_;
-    IonCompileTaskPtrVector         finishedTasks_;
 
     // Assertions
     DebugOnly<FunctionGenerator*>   activeFuncDef_;
